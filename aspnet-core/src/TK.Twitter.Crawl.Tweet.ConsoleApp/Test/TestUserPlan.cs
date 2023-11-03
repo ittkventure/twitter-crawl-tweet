d5b24e8977d63fe5a0571d25918b2382188051f5
haihp <hoangphihai93@gmail.com>
2023-11-03 10:30:51 +0700

    Crawl dữ liệu tweet liên tục

--- aspnet-core/src/TK.Twitter.Crawl.Tweet.ConsoleApp/Test/TestUserPlan.cs
+++ aspnet-core/src/TK.Twitter.Crawl.Tweet.ConsoleApp/Test/TestUserPlan.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TK.Twitter.Crawl.Entity;
+using TK.Twitter.Crawl.Jobs;
 using TK.Twitter.Crawl.Tweet.Payment;
+using TK.Twitter.Crawl.Tweet.TwitterAPI.Dto.FollowingCrawl;
+using TK.TwitterAccount.Domain.Entities;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Emailing;
@@ -13,15 +18,55 @@ namespace TK.Twitter.Crawl.ConsoleApp.Test
         private readonly IRepository<UserPlanEntity, Guid> _userPlanRepository;
         private readonly IEmailSender _emailSender;
         private readonly PaddleAfterWebhookLogAddedHandler _paddleAfterWebhookLogAddedHandler;
+        private readonly AirTableManualSourceProcessWaitingJob _airTableManualSourceProcessWaitingJob;
+        private readonly IRepository<TwitterAccountEntity, Guid> _twitterAccountRepository;
 
-        public TestUserPlan(IRepository<UserPlanEntity, Guid> userPlanRepository, IEmailSender emailSender, PaddleAfterWebhookLogAddedHandler paddleAfterWebhookLogAddedHandler)
+        public TestUserPlan(IRepository<UserPlanEntity, Guid> userPlanRepository, IEmailSender emailSender, PaddleAfterWebhookLogAddedHandler paddleAfterWebhookLogAddedHandler, AirTableManualSourceProcessWaitingJob airTableManualSourceProcessWaitingJob,
+            IRepository<TwitterAccountEntity, Guid> twitterAccountRepository)
         {
             _userPlanRepository = userPlanRepository;
             _emailSender = emailSender;
             _paddleAfterWebhookLogAddedHandler = paddleAfterWebhookLogAddedHandler;
+            _airTableManualSourceProcessWaitingJob = airTableManualSourceProcessWaitingJob;
+            _twitterAccountRepository = twitterAccountRepository;
         }
 
         public async Task RunAsync()
+        {
+            try
+            {
+                var urls = GetUrls();
+                var currentAcc = await _twitterAccountRepository.FirstOrDefaultAsync(x => x.Enabled == true);
+                var list = new List<TwitterUserDto>();
+
+                // https://twitter.com/qiaoyunzi1/status/1658420894946181120?s=20
+                // https://twitter.com/traderrocko/status/1658108709644402699
+
+                foreach (var item in urls)
+                {
+                    var data = await _airTableManualSourceProcessWaitingJob.GetTwitterUserAsync(item, currentAcc);
+                    if (data != null)
+                    {
+                        list.Add(data);
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+
+                var list1 = list.Select(x => new { Id = $"'{x.Id}", Name = x.Name, ScreenName = x.ScreenName }).ToList();
+
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+        }
+
+        public async Task RunAsync_SendEmailWelcome()
         {
             try
             {
@@ -32,6 +77,13 @@ namespace TK.Twitter.Crawl.ConsoleApp.Test
 
 
             }
+        }
+
+        private static List<string> GetUrls()
+        {
+            return new List<string>() {
+                "https://twitter.com/jasmy_BNB","https://twitter.com/wolongkm","https://twitter.com/BddVenture","https://twitter.com/FungiAlpha","https://twitter.com/thedefiedge","https://twitter.com/doomsdart","https://twitter.com/Wuhuoqiu","https://twitter.com/qiaoyunzi1","https://twitter.com/huahuayjy","https://twitter.com/traderrocko","https://twitter.com/ViktorDefi","https://twitter.com/FatManWithGems","https://twitter.com/0xKofi","https://twitter.com/korpi87","https://twitter.com/thewolfofdefi","https://twitter.com/corleonescrypto","https://twitter.com/MauLu5630739070","https://twitter.com/peach_1340","https://twitter.com/tztokchad","https://twitter.com/CryptoDamus411","https://twitter.com/ZoomerOracle","https://twitter.com/CryptoGideon_","https://twitter.com/myBlockBrain","https://twitter.com/563defi","https://twitter.com/Hercules_Defi","https://twitter.com/francescoweb3","https://twitter.com/CryptoNikyous","https://twitter.com/PaikCapital","https://twitter.com/notnotstorm","https://twitter.com/quant_arb","https://twitter.com/HubertX13","https://twitter.com/RuggedWojak","https://twitter.com/apes_prologue","https://twitter.com/DefiIgnas","https://twitter.com/AlphaSeeker21","https://twitter.com/Gemhunter9000","https://twitter.com/alphascan_xyz","https://twitter.com/DegenSensei","https://twitter.com/0xSami_","https://twitter.com/IamMusteee","https://twitter.com/ChrisRomanoC","https://twitter.com/EskManso","https://twitter.com/QuantMeta","https://twitter.com/CryptoYusaku"
+            };
         }
     }
 }
--- aspnet-core/src/TK.Twitter.Crawl.Tweet.Domain/BackgroundWorker/TwitterTweetCrawlWorker.cs
+++ aspnet-core/src/TK.Twitter.Crawl.Tweet.Domain/BackgroundWorker/TwitterTweetCrawlWorker.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using TK.Twitter.Crawl.Entity;
 using TK.Twitter.Crawl.Jobs;
 using Volo.Abp.BackgroundJobs;
@@ -13,26 +15,20 @@ namespace TK.Twitter.Crawl.BackgroundWorkers
     public class TwitterTweetCrawlWorker : ITransientDependency
     {
         private readonly IBackgroundJobManager _backgroundJobManager;
-        private readonly IRepository<TwitterTweetCrawlBatchEntity, long> _twitterTweetCrawlBatchRepository;
-        private readonly IRepository<TwitterTweetCrawlQueueEntity, long> _twitterTweetCrawlQueueRepository;
         private readonly IUnitOfWorkManager _unitOfWorkManager;
-        private readonly IDistributedEventBus _distributedEventBus;
+        private readonly IRepository<TwitterTweetCrawlQueueEntity, long> _twitterTweetCrawlQueueRepository;
 
         public TwitterTweetCrawlWorker(
             IBackgroundJobManager backgroundJobManager,
-            IRepository<TwitterTweetCrawlBatchEntity, long> twitterTweetCrawlHistoryRepository,
             IRepository<TwitterTweetCrawlQueueEntity, long> twitterTweetCrawlQueueRepository,
             IClock clock,
-            IUnitOfWorkManager unitOfWorkManager,
-            IDistributedEventBus distributedEventBus
+            IUnitOfWorkManager unitOfWorkManager
             )
         {
             _backgroundJobManager = backgroundJobManager;
-            _twitterTweetCrawlBatchRepository = twitterTweetCrawlHistoryRepository;
-            _twitterTweetCrawlQueueRepository = twitterTweetCrawlQueueRepository;
             Clock = clock;
             _unitOfWorkManager = unitOfWorkManager;
-            _distributedEventBus = distributedEventBus;
+            _twitterTweetCrawlQueueRepository = twitterTweetCrawlQueueRepository;
         }
 
         public IClock Clock { get; }
@@ -41,10 +37,28 @@ namespace TK.Twitter.Crawl.BackgroundWorkers
         public async Task DoWorkAsync()
         {
             using var uow = _unitOfWorkManager.Begin();
-
             try
             {
-                await StartCrawl();
+                var waitingProcesses = await _twitterTweetCrawlQueueRepository.GetListAsync(x => x.Ended == false);
+                if (waitingProcesses.IsEmpty())
+                {
+                    await StartCrawl();
+                }
+                else
+                {
+                    var groups = waitingProcesses.GroupBy(x => new { x.BatchKey, x.TwitterAccountId })
+                                                 .Select(x => new { x.Key.BatchKey, x.Key.TwitterAccountId })
+                                                 .OrderBy(x => x.BatchKey);
+                    foreach (var grp in groups)
+                    {
+                        await _backgroundJobManager.EnqueueAsync(new TwitterTweetCrawlJobArg()
+                        {
+                            BatchKey = grp.BatchKey,
+                            TwitterAccountId = grp.TwitterAccountId,
+                        });
+                    }
+                }
+
                 await uow.SaveChangesAsync();
                 await uow.CompleteAsync();
             }
@@ -53,7 +67,6 @@ namespace TK.Twitter.Crawl.BackgroundWorkers
                 await uow.RollbackAsync();
                 throw;
             }
-
         }
 
         private Task StartCrawl()
